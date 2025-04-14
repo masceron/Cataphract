ec03369e1c44c204ab067d7df00e6aa75992cf93
VaguePasta <qphuc21158@gmail.com>
2025-04-14 21:05:00 +0700

    Added buckets to transposition table

--- eval/transposition.hpp
+++ eval/transposition.hpp
@@ -1,11 +1,6 @@
 #pragma once
 
 #include <cstdint>
-
-#ifdef _MSC_VER
-#include <intrin.h>
-#endif
-
 #include "move.hpp"
 
 enum Values: int16_t
@@ -22,54 +17,76 @@ enum NodeType: uint8_t
     pv_node, all_node, cut_node
 };
 
+inline constexpr uint8_t age_delta = 4;
+
 struct Entry
 {
-    uint64_t key;
+    uint16_t key;
     Move best_move;
     uint8_t depth;
     int16_t score;
-    NodeType type;
+    uint8_t age_type;
 };
 
-constexpr uint16_t table_size_in_mb = 128;
+struct Bucket
+{
+    Entry entries[3];
+};
+
+static constexpr uint64_t table_size = 8388608;
 
 namespace TT
 {
-    inline auto table = new std::array<Entry, table_size_in_mb * (1024 * 1024) / sizeof(Entry)>();
-    inline uint64_t size = table->size();
+    inline auto table = new std::array<Bucket, table_size>();
+    inline uint8_t age = 0;
 
     inline uint64_t index_of(const uint64_t& key)
     {
-        uint64_t index;
-    #ifdef __GNUC__
-        const __uint128_t prod = static_cast<__uint128_t>(key) * size;
-        index = prod >> 64;
-    #elif _MSC_VER
-        index = __umulh(key, size);
-    #endif
-        return index;
+        static constexpr uint64_t multiplier = 0x9e3779b97f4a7c15ull;
+        return (key * multiplier) & (table_size - 1);
     }
 
-    inline Entry* probe(const uint64_t key, bool& ok)
+    inline std::pair<Bucket*, Entry*> probe(const uint64_t key, bool& ok)
     {
         const uint64_t index = index_of(key);
+        const auto entry_key = static_cast<uint16_t>(key);
 
-        if ((*table)[index].key != key) {
-            ok = false;
+        for (auto& entry : (*table)[index].entries) {
+            if (entry.key == entry_key) {
+                return {&(*table)[index], &entry};
+            }
         }
-        return &(*table)[index];
+        ok = false;
+        return {&(*table)[index], nullptr};
     }
 
-    inline void write(Entry* entry, const uint64_t key, const Move best_move, const uint8_t depth, int16_t score, const NodeType type)
+    inline void write(Bucket* bucket, Entry* entry, const uint64_t key, const Move best_move, const uint8_t depth, int16_t score, const NodeType type)
     {
         if (score < -mate_bound) score -= depth;
         else if (score > mate_bound) score += depth;
 
-        *entry = Entry(key, best_move, depth, score, type);
+        const auto entry_key = static_cast<uint16_t>(key);
+        if (entry != nullptr) {
+            *entry = Entry(entry_key, best_move, depth, score, type + age);
+            return;
+        }
+
+        uint8_t shallowest = 0;
+
+        for (int8_t i = 0; i < 3; i++) {
+            if ((bucket->entries[i].age_type & 252) != age) {
+                bucket->entries[i] = Entry(entry_key, best_move, depth, score, type + age);
+                return;
+            }
+            if (bucket->entries[i].depth < bucket->entries[shallowest].depth) shallowest = i;
+        }
+
+        bucket->entries[shallowest] = Entry(entry_key, best_move, depth, score, type + age);
     }
 
     inline void clear()
     {
-        std::memset(&(*table)[0], 0, table->size() * sizeof(Entry));
+        age = 0;
+        memset(&table[0], 0, table_size * sizeof(Bucket));
     }
 }

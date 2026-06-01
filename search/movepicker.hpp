286044696d09f4e9f28d8a08c103e0c9b528b49d
Masceron <51254101+masceron@users.noreply.github.com>
2026-06-01 10:31:08 +0700

    Sort bad captures with SEE

--- search/movepicker.hpp
+++ search/movepicker.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <utility>
 
 #include "history.hpp"
 #include "../position/movegen.hpp"
@@ -30,10 +31,12 @@ struct MovePicker
     SearchThread& thread;
     SearchEntry* ss;
     Move pv = null_move;
-    Move* non_captures_start;
-    Move* current;
-    Move* bad_captures_end;
     MoveList moves;
+
+    int current = 0;
+    int end = 0;
+    int bad_captures_end = 255;
+
     int threshold;
     int scores[256];
     bool noisy_only;
@@ -42,8 +45,9 @@ struct MovePicker
     explicit MovePicker(SearchThread& _thread, const bool _noisy_only, const Move _pv, SearchEntry* _ss,
                         const int _threshold = 0) : thread(_thread), ss(_ss), noisy_only(_noisy_only)
     {
-        bad_captures_end = &moves.list[255];
+        bad_captures_end = 255;
         threshold = _threshold;
+
         if (thread.position.state->attacks == UINT64_MAX)
         {
             thread.position.get_attacks();
@@ -72,26 +76,30 @@ struct MovePicker
         case Stage::TT_moves:
             stage = Stage::generating_capture_moves;
             return {pv, 0};
+
         case Stage::generating_capture_moves:
             stage = Stage::good_capture_moves;
             pseudo_legals<MoveType::noisy>(pos, moves);
-            score_mvv_caphist(moves.begin(), moves.last);
-            non_captures_start = moves.last;
-            current = moves.begin();
+            end = moves.last - moves.begin();
+            score_mvv_caphist(0, end);
+            current = 0;
             [[fallthrough]];
+
         case Stage::good_capture_moves:
-            while (current < non_captures_start)
+            while (current < end)
             {
-                select_highest(current, non_captures_start);
-                Move move = *current;
-                auto score = scores[current - moves.begin()];
+                select_highest(current, end);
+                Move move = moves.list[current];
+                auto score = scores[current];
                 current++;
 
-                if (move == pv) continue; // Skip TT move
+                if (move == pv) continue;
 
-                if (static_exchange_evaluation(thread.position, move) < threshold)
+                if (const auto exc = static_exchange_evaluation(thread.position, move); exc < threshold)
                 {
-                    *bad_captures_end-- = move;
+                    moves.list[bad_captures_end] = move;
+                    scores[bad_captures_end] = exc;
+                    bad_captures_end--;
                     continue;
                 }
 
@@ -99,27 +107,33 @@ struct MovePicker
             }
             stage = Stage::killer_1;
             [[fallthrough]];
+
         case Stage::killer_1:
             stage = Stage::killer_2;
-            if (const Move killer = history.killers.get(ss->plies, 0); killer != pv && thread.position.is_pseudo_legal(killer))
+            if (const Move killer = history.killers.get(ss->plies, 0); killer != pv && thread.position.
+                is_pseudo_legal(killer))
             {
                 return {killer, UINT16_MAX};
             }
             [[fallthrough]];
+
         case Stage::killer_2:
             stage = Stage::generating_quiet_moves;
-            if (const Move killer = history.killers.get(ss->plies, 1); killer != pv && thread.position.is_pseudo_legal(killer))
+            if (const Move killer = history.killers.get(ss->plies, 1); killer != pv && thread.position.
+                is_pseudo_legal(killer))
             {
                 return {killer, UINT16_MAX};
             }
             [[fallthrough]];
+
         case Stage::generating_quiet_moves:
             if (!noisy_only)
             {
+                current = end;
                 pseudo_legals<MoveType::quiet>(thread.position, moves);
-                score_history(non_captures_start, moves.last);
+                end = moves.last - moves.begin();
+                score_history(current, end);
                 stage = Stage::quiet_moves;
-                current = non_captures_start;
             }
             else
             {
@@ -127,12 +141,13 @@ struct MovePicker
                 return {null_move, 0};
             }
             [[fallthrough]];
+
         case Stage::quiet_moves:
-            while (current < moves.last)
+            while (current < end)
             {
-                select_highest(current, moves.last);
-                Move move = *current;
-                auto score = scores[current - moves.begin()];
+                select_highest(current, end);
+                Move move = moves.list[current];
+                auto score = scores[current];
                 current++;
 
                 if (move == pv ||
@@ -146,20 +161,30 @@ struct MovePicker
             }
 
             stage = Stage::bad_capture_moves;
-            current = &moves.list[255];
+            current = bad_captures_end + 1;
+            end = 256;
             [[fallthrough]];
+
         case Stage::bad_capture_moves:
-            while (current > bad_captures_end)
+            while (current < end)
             {
-                Move move = *current;
-                current--;
-                return {move, -1};
+                select_highest(current, end);
+                Move move = moves.list[current];
+                auto score = scores[current];
+                current++;
+                return {move, score};
             }
             stage = Stage::none;
             [[fallthrough]];
+
         case Stage::none: default:
             return {null_move, 0};
         }
+    }
+
+    void skip_quiets()
+    {
+        current = end;
     }
 
     std::pair<Move, int> next_move()
@@ -176,19 +201,19 @@ struct MovePicker
         return move;
     }
 
-    void score_mvv_caphist(const Move* begin, const Move* end)
+    void score_mvv_caphist(int start_idx, int end_idx)
     {
-        if (begin == end) return;
+        if (start_idx == end_idx) return;
 
         const auto& pos = thread.position;
         const auto& history = thread.history;
         const bool stm = pos.side_to_move;
 
-        for (const Move* move = begin; move < end; ++move)
+        for (int i = start_idx; i < end_idx; ++i)
         {
-            const auto index = move - begin;
-            const auto from = move->from();
-            const auto to = move->to();
+            Move move = moves.list[i];
+            const auto from = move.from();
+            const auto to = move.to();
 
             int moved = pos.piece_on[from];
             int captured = pos.piece_on[to];
@@ -197,33 +222,34 @@ struct MovePicker
             else if (captured >= 6) captured -= 6;
             if (moved >= 6) moved -= 6;
 
-            scores[index] = mvv[captured] * mvv_weight() / 1024 + history.capture.table[stm][moved][captured][to] *
+            scores[i] = mvv[captured] * mvv_weight() / 1024 + history.capture.table[stm][moved][captured][to] *
                 capture_history_weight() / 1024;
-            if (move->flag() >= MoveFlag::knight_promo_capture)
-                scores[index] = scores[index]
-                    + value_of(move->promoted_to());
+
+            if (move.flag() >= MoveFlag::knight_promo_capture)
+                scores[i] = scores[i] + value_of(move.promoted_to());
         }
     }
 
-    void score_history(const Move* begin, const Move* end)
+    void score_history(int start_idx, int end_idx)
     {
-        if (begin == end) return;
+        if (start_idx == end_idx) return;
 
-        const auto offset = non_captures_start - moves.begin();
         const auto prev = (ss - 1)->piece_to != UINT16_MAX ? (ss - 1)->piece_to : 0;
         const auto prev2 = (ss - 2)->piece_to != UINT16_MAX ? (ss - 2)->piece_to : 0;
         const auto prev4 = (ss - 4)->piece_to != UINT16_MAX ? (ss - 4)->piece_to : 0;
         const auto& pos = thread.position;
         const auto& history = thread.history;
 
-        for (const Move* move = begin; move < end; ++move)
+        for (int i = start_idx; i < end_idx; ++i)
         {
-            const auto index = offset + (move - begin);
-            const auto from = move->from();
-            const auto to = move->to();
+            Move move = moves.list[i];
+            const auto from = move.from();
+            const auto to = move.to();
+
             int moved = pos.piece_on[from];
             if (moved >= 6) moved -= 6;
-            scores[index] =
+
+            scores[i] =
                 (history.butterfly_history.table[pos.side_to_move][from][to]
                     + history.piece_to_history.table[pos.side_to_move][moved][to]) * piece_history_weight() / 1024
                 + history.continuation.counter_moves[pos.side_to_move][prev >> 6][prev & 0b111111][moved][to] *
@@ -235,21 +261,21 @@ struct MovePicker
         }
     }
 
-    void select_highest(Move* start, const Move* end)
+    void select_highest(int start_idx, int end_idx)
     {
-        Move* best = start;
-        auto best_score = scores[start - moves.begin()];
+        int best_idx = start_idx;
+        auto best_score = scores[start_idx];
 
-        for (Move* it = start + 1; it < end; ++it)
+        for (int i = start_idx + 1; i < end_idx; ++i)
         {
-            if (const auto score = scores[it - moves.begin()]; score > best_score)
+            if (const auto score = scores[i]; score > best_score)
             {
                 best_score = score;
-                best = it;
+                best_idx = i;
             }
         }
 
-        std::swap(*start, *best);
-        std::swap(scores[start - moves.begin()], scores[best - moves.begin()]);
+        std::swap(moves.list[start_idx], moves.list[best_idx]);
+        std::swap(scores[start_idx], scores[best_idx]);
     }
 };

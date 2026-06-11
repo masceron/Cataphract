8d8894ac4fbc4a62004600a11ad762630a167796
Masceron <51254101+masceron@users.noreply.github.com>
2026-06-11 16:09:15 +0700

    Use C++23 string_views

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <ranges>
 #include "uci.hpp"
 #include "engine.hpp"
 
@@ -7,12 +8,14 @@ int main(const int argc, char* argv[])
 
     if (argc > 1 && std::string_view(argv[1]) == "bench")
     {
-        std::string bench_args;
-        for (int i = 1; i < argc; ++i)
-        {
-            bench_args += argv[i];
-            if (i < argc - 1) bench_args += " ";
-        }
+        const std::string bench_args = std::span(argv, argc)
+            | std::views::drop(1)
+            | std::views::transform([](const char* ptr)
+            {
+                return std::string_view(ptr);
+            })
+            | std::views::join_with(' ')
+            | std::ranges::to<std::string>();
 
         UCI::bench(bench_args);
         return 0;
